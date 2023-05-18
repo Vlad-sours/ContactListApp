705e5f8b04184952de11b9971679561b54589b07
Vladimir <xalif80@gmail.com>
2023-05-18 12:48:07 +0200

    add class DataStore

--- ContactListApp/Servises/DataStore.swift
+++ ContactListApp/Servises/DataStore.swift
@@ -6,3 +6,102 @@
 //
 
 import Foundation
+class DataStore{
+    let names = [
+        "Abraham",
+        "Adam",
+        "Adrian",
+        "Albert",
+        "Alexander",
+        "Alfred",
+        "Anderson",
+        "Andrew",
+        "Anthony",
+        "Arnold",
+        "Oliver",
+        "Jack",
+        "Harry",
+        "Jacob",
+        "Charley",
+        "Thomas",
+        "George",
+        "Oscar",
+        "James",
+        "William"
+    ]
+    
+    let lastNames = [
+        "Calhoun",
+        "Campbell",
+        "Campion",
+        "Cantwell",
+        "Carey",
+        "Carrington",
+        "Carroll",
+        "Charles",
+        "Carter",
+        "Chandler",
+        "Chapman",
+        "Charlson",
+        "Chesterton",
+        "Dickinson",
+        "Dodson",
+        "Dodwell",
+        "Donaldson",
+        "Donovan",
+        "Doubleday",
+        "Douglas",
+        "Dowman",
+        "Downing"
+    ]
+    
+    let phoneNumbers = [
+        "+7(916) 000-1980",
+        "+7(916) 001-1980",
+        "+7(916) 030-1980",
+        "+7(916) 000-1990",
+        "+7(986) 000-1980",
+        "+7(916) 070-1980",
+        "+7(916) 000-1981",
+        "+7(916) 000-1982",
+        "+7(916) 000-1983",
+        "+7(916) 000-1380",
+        "+7(916) 000-19000",
+        "+7(916) 008-1980",
+        "+7(9966) 000-1980",
+        "+7(916) 120-1980",
+        "+7(916) 000-1980",
+        "+7(916) 000-1979",
+        "+7(916) 000-1180",
+        "+7(916) 000-19560",
+        "+7(916) 000-199080",
+        "+7(916) 000-1980",
+        "+7(616) 000-1980",
+        "+7(116) 000-1980",
+    ]
+    
+    let emails = [
+        "sasha@list.ru",
+        "tomochka72@inbox.ru",
+        "anya@bk.ru",
+        "rit86@mail.ru",
+        "all.92@inbox.ru",
+        "lyu91@bk.ru",
+        "ir1@inbox.ru",
+        "ma7@inbox.ru",
+        "ni.83@bk.ru",
+        "elova97_97@list.ru",
+        "poa88_88@inbox.ru",
+        "yerova.65@bk.ru",
+        "vittsev973@inbox.ru",
+        "eleonilova.92@bk.ru",
+        "mashdrova.60@list.ru",
+        "kat7272@bk.ru",
+        "evelia.72@bk.ru",
+        "masheva.71@bk.ru",
+        "yevgva_63@inbox.ru",
+        "viva_1996@list.ru",
+        "valova-84@inbox.ru",
+        "rimva_78@bk.ru"
+    ]
+}
